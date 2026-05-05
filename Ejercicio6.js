c6a72dfb96a5a20443fc0c04e795119333b11ac0
elrandal <elvis.randal.quintanilla@clases.edu.sv>
2026-05-05 15:43:45 -0600

    Create Ejercicio6.js

---
+++ Ejercicio6.js
@@ -0,0 +1,7 @@
+// Usamos un bucle for:
+// 1. Iniciamos en 1 (let i = 1)
+// 2. Condición: mientras i sea menor o igual a 10 (i <= 10)
+// 3. Incremento: sumamos 1 en cada vuelta (i++)
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
