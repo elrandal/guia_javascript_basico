b9933067795e8dd5df5981a3d6cab7babdf88aab
elrandal <elvis.randal.quintanilla@clases.edu.sv>
2026-05-05 15:39:16 -0600

    Create Ejercicio4.js

---
+++ Ejercicio4.js
@@ -0,0 +1,11 @@
+let nota = 7;
+
+if (nota >= 9) {
+  console.log('Excelente');
+} else if (nota >= 7) {
+  console.log('Bueno');
+} else if (nota >= 5) {
+  console.log('Regular');
+} else {
+  console.log('Reprobado');
+}
