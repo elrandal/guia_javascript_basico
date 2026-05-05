7c0459782c6261f83894937e0561b134c1968a17
elrandal <elvis.randal.quintanilla@clases.edu.sv>
2026-05-05 15:50:35 -0600

    Create Ejercicio9.js

---
+++ Ejercicio9.js
@@ -0,0 +1,9 @@
+
+for (let i = 1; i <= 15; i++) {
+  // El símbolo % calcula el resto de la división por 2
+  if (i % 2 === 0) {
+    console.log(`${i} es par`);
+  } else {
+    console.log(`${i} es impar`);
+  }
+}
