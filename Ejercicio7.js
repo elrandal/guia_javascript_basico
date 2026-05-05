783e3213080b70e97ff275e1aa8367507e2b9d97
elrandal <elvis.randal.quintanilla@clases.edu.sv>
2026-05-05 15:46:56 -0600

    Create Ejercicio7.js

---
+++ Ejercicio7.js
@@ -0,0 +1,8 @@
+let suma = 0;
+
+for (let i = 1; i <= 100; i++) {
+  // Sumamos el valor actual de 'i' a nuestra alcancía 'suma'
+  suma = suma + i; 
+}
+
+console.log(`La suma de 1 a 100 es: ${suma}`);
