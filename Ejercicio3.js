cdd8449f72f65c76d763dd3b02e6cf2884fce88e
elrandal <elvis.randal.quintanilla@clases.edu.sv>
2026-05-05 15:35:46 -0600

    Create Ejercicio3.js

---
+++ Ejercicio3.js
@@ -0,0 +1,7 @@
+let edad = 16; // Cambia este valor para probar (ej: 18 o 25)
+
+if (edad >= 18) {
+  console.log('La persona es mayor de edad.');
+} else {
+  console.log('La persona es menor de edad.');
+}
